c977f8492aca4021a3f7ee9178b2cd09710dd51d
ivitec <ivi.tec1983@gmail.com>
2024-07-25 22:17:13 -0600

    init

---
+++ index2.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Ivitec</title>
+    <link href="./libs/bootstrap-5.3.3-dist/css/bootstrap.css" rel="stylesheet">
+    <link href="./css/layout.css" rel="stylesheet">
+</head>
+<body>
+<div class="accordion accordion-flush" id="accordionFlushExample">
+    <div class="accordion-item border-0">
+        <h2 class="accordion-header" id="flush-headingOne">
+            <button class="accordion-button collapsed bg-black text-white" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                <img class="icono" src="casita.blanca.webp">
+                Bienvenidos
+            </button>
+        </h2>
+        <div id="flush-collapseOne" class="accordion-collapse collapse min-vh-100 bg-black text-white" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+            <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the first item's accordion body.</div>
+        </div>
+    </div>
+    <div class="accordion-item border-0">
+        <h2 class="accordion-header" id="flush-headingTwo">
+            <button class="accordion-button collapsed bg-black text-white" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+                Que te parece si nos conocemos?
+            </button>
+        </h2>
+        <div id="flush-collapseTwo" class="accordion-collapse collapse min-vh-100 bg-black text-white" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
+            <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the second item's accordion body. Let's imagine this being filled with some actual content.</div>
+        </div>
+    </div>
+    <div class="accordion-item border-0">
+        <h2 class="accordion-header" id="flush-headingThree">
+            <button class="accordion-button collapsed bg-black text-white" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+                Acompañame y te muestro
+            </button>
+        </h2>
+        <div id="flush-collapseThree" class="accordion-collapse collapse min-vh-100 bg-black text-white" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
+            <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the third item's accordion body. Nothing more exciting happening here in terms of content, but just filling up the space to make it look, at least at first glance, a bit more representative of how this would look in a real-world application.</div>
+        </div>
+    </div>
+</div>
+<script src="./libs/bootstrap-5.3.3-dist/js/popper.js" ></script>
+<script src="./libs/bootstrap-5.3.3-dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
